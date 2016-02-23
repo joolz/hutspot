2ad4ec771e8d0bd8e710a2dd97e5b62df2d3a959
joolz <joolz@xs4all.nl>
2016-02-23 16:22:24 +0100

    Script to mail part of catalina.out after portlet deploy

---
+++ liferay/maildeploylog.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Get the part of the tomcat log starting with the deployment, up until the end and mail it.
+
+if [ -z "$1" ]; then
+	logger "$0 called without parameters"
+	echo Usage: $0 projectname mailto_address
+	echo
+	echo For example: $0 nl-ou-dlwo-fubar-portlet
+	exit 1
+fi
+
+if [ -z "$2" ]; then
+	logger "$0 called with too little parameters"
+	echo Usage: $0 projectname mailto_address
+	echo
+	echo For example: $0 nl-ou-dlwo-fubar-portlet
+	exit 1
+fi
+
+LOGFILE=/usr/local/liferay/tomcat/logs/catalina.out
+PROJECTSTRING="$1"
+MAILTO="$2"
+DEPLOYSTRING="^[0-9].*Processing $PROJECTSTRING.*war$"
+
+FOUND=`grep -n -m1 -e "$DEPLOYSTRING" $LOGFILE`
+if [ -z "$FOUND" ]; then
+	logger "$DEPLOYSTRING not found in $LOGFILE"
+	exit 1
+fi
+
+LINE_NUMBER=`echo $FOUND | cut -d : -f 1`
+
+LOGPART=`cat $LOGFILE | awk "NR >= $LINE_NUMBER"`
+
+echo "$LOGPART" | mailx -S "smtp=mail.lokaal" -s "deploy log of $1" $MAILTO
+
---
+++ upgrade.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get update && \
+	sudo apt-get dist-upgrade -y && \
+	sudo apt-get autoremove -y
