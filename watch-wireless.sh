52bddc212c5d42e92d94b3c6e0e07e4f1cc6b425
joolz <joolz@xs4all.nl>
2016-04-08 12:45:37 +0200

    Add ping check

--- watch-wireless.sh
+++ watch-wireless.sh
@@ -3,6 +3,7 @@
 LOGFILE=/var/log/watch-wireless.log
 INTERVAL=5s
 INTERFACE=wlan0
+PINGEXTERNALHOST=www.xs4all.nl
 
 while [ 0 ]; do
   DATESTAMP=`date +%Y-%m-%d\ %H:%M:%S`
@@ -11,7 +12,12 @@ while [ 0 ]; do
 	RX=`ifconfig $INTERFACE | grep "RX packets:"`
 	TX=`ifconfig $INTERFACE | grep "TX packets:"`
 	COLLISIONS=`ifconfig $INTERFACE | grep "collisions:"`
-  echo $DATESTAMP: $IP_ADDRESS - $LINK $RX $TX $COLLISIONS >> $LOGFILE
+
+	ping -c 1 -W 4 $PINGEXTERNALHOST &> /dev/null
+	PINGEXTERNALSTATUS=$?
+
+  echo $DATESTAMP: $IP_ADDRESS / $LINK / $RX / $TX / $COLLISIONS / \
+		ping $PINGEXTERNALHOST status $PINGEXTERNALSTATUS >> $LOGFILE
   sleep $INTERVAL
 done
 
