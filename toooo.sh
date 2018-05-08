a96776a4888b74016478308ab8c9a59dcbbd490e
joolz <joolz@xs4all.nl>
2018-05-08 15:56:36 +0200

    Brutally convert to odt

---
+++ toooo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+libreoffice --headless --convert-to odt *.doc
+libreoffice --headless --convert-to odt *.docx
+libreoffice --headless --convert-to ods *.xls
+libreoffice --headless --convert-to ods *.xlsx
+libreoffice --headless --convert-to odp *.ppt
+libreoffice --headless --convert-to odp *.pptx
