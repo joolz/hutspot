07f6f5651d51eb3929595a411a494a4762e0495f
joolz <joolz@xs4all.nl>
2018-05-14 13:48:00 +0200

    Wrapper script to patch DXP, workaround for OUNDLWO-118

---
+++ patchdxp.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# workaround script for https://web.liferay.com/group/customer/support/-/support/ticket/OUNDLWO-118
+
+source ~/bin/common.sh || exit 1
+source $CREDSFILE || exit 1
+
+liferayrunningcheck
+
+case "$1" in
+
+"source")
+	confirm "Will patch DXP sources. Continue?"
+	pushd $DXPSERVERDIR/patching-tool || exit 1
+	rm patches/* || exit 1
+	cp $DXPPATCHESDIR/source/* patches/
+	cp $DXPPATCHESDIR/combined/* patches/
+	cp source.properties default.properties
+	./patching-tool.sh install
+	rm default.properties
+	popd
+	;;
+
+"binary")
+	confirm "Will patch DXP binaries. Continue?"
+	pushd $DXPSERVERDIR/patching-tool || exit 1
+	rm patches/* || exit 1
+	cp $DXPPATCHESDIR/binary/* patches/
+	cp $DXPPATCHESDIR/combined/* patches/
+	cp binary.properties default.properties
+	./patching-tool.sh install
+	rm default.properties
+	popd
+	;;
+
+*)
+	echo "Usage: $0 [source|binary]"
+	;;
+
+esac
+
