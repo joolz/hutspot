9850abd607162bdca4ceab427a84356e94a57efe
joolz <joolz@xs4all.nl>
2018-06-08 07:49:39 +0200

    Improve jrebel check

--- rebelcheck.sh
+++ rebelcheck.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-DIRS="~/workspace ~/workspace-adhoc /opt/liferay/portal/tomcat-7.0.62"
+PATTERN="/home/jal/workspace"
+DIRS="$PATTERN /opt/dxp/server/tomcat-8.0.32"
 
 for I in $DIRS; do
-	find $I -name rebel.xml -exec grep -L "<dir name=\"~/workspace" {} \;
+	find $I -name rebel.xml -exec grep -L "<dir name=\"$PATTERN" {} \;
 done
 
