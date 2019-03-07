ccac817ec25b45ed2c734bfc22f8f7574a889a37
joolz <joolz@xs4all.nl>
2019-03-07 10:14:49 +0100

    Compat with older bash

--- utf_check_properties.sh
+++ utf_check_properties.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 UTF="UTF-8"
+declare ERRORS
+ERRORS=""
 
-shopt -s globstar
-for FILE in **/*.properties; do
+find . -name '*.properties' -print0 | while IFS= read -r -d $'\0' FILE; do
 	ENCODING=`file -b ${FILE} | awk -F " " '{print $1}'`
 
 	FULLPATH=`readlink -f ${FILE}`
 	echo ${FULLPATH} has encoding ${ENCODING}
 
 	if [ "${ENCODING}" != "${UTF}" ]; then
-		ERRORS="true"
+		false
+		exit
 	fi
 done
 
-if [ "${ERRORS}" == "true" ]; then
+if [ $? -ne 0 ]; then
 	echo "ERROR: not all properties files are ${UTF-8}"
 	exit 1
 fi
