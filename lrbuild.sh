64b1b8fef973c62ee5795437e7864af5d7670108
joolz <joolz@xs4all.nl>
2019-03-29 13:11:47 +0100

    Check encoding before building

--- lrbuild.sh
+++ lrbuild.sh
@@ -3,8 +3,31 @@
 source ~/bin/common.sh || exit 1
 source $CREDSFILE || exit 1
 
+UTF="UTF-8"
+ASCII="ASCII"
+HTML="HTML"
+ERRORFILE="ERRORFILE.removethis"
+
 if [ ! -f pom.xml ]; then
 	echo No pom
+	exit 1
+fi
+
+find . -name 'Language*.properties' -print0 | while IFS= read -r -d $'\0' FILE; do
+	ENCODING=`file -b ${FILE} | awk -F " " '{print $1}'`
+
+	FULLPATH=`readlink -f ${FILE}`
+	echo ${FULLPATH} has encoding ${ENCODING}
+
+	if [ "${ENCODING}" != "${UTF}" ] && [ "${ENCODING}" != "${ASCII}" ] && [ "${ENCODING}" != "${HTML}" ]; then
+		touch ${ERRORFILE} # one way to get out of a piped loop and check the condition
+		exit
+	fi
+done
+
+if [ -f ${ERRORFILE} ]; then
+	rm ${ERRORFILE}
+	echo "Encountered an error"
 	exit 1
 fi
 
