a8db0fb0af38b30a712cb948c1805ec8d00364ef
joolz <joolz@xs4all.nl>
2019-04-01 13:33:33 +0200

    Cleanup after kill

--- killtomcat.sh
+++ killtomcat.sh
@@ -7,6 +7,7 @@ TOMCATPID=`jps -l | grep org.apache.catalina.startup.Bootstrap | awk '{print $1}
 if [ "$TOMCATPID" != "" ]; then
 	confirm "Kill tomcat PID $TOMCATPID ?"
 	kill -9 $TOMCATPID
+	liferaycleanup
 else
 	echo "No running tomcat detected"
 fi
