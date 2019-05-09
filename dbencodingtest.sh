8895d74ae7199ebc39e46d3e573c2e8a4baa7cd6
joolz <joolz@xs4all.nl>
2019-05-09 14:54:59 +0200

    Smiley / UTF8 sampler

---
+++ dbencodingtest.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# see http://www.unicode.org/emoji/charts/full-emoji-list.html
+SMILEYS="😀🤣🤪🤔🤮😱💀💩👽💌💣👰💃🏄🦁🦃🐲🎾♒📛🈂⚫🏴☠️🏴"
+
+# see http://www.columbia.edu/~fdc/utf8/
+RUNE="ᚠᛇᚻ᛫ᛒᛦᚦ᛫ᚠᚱᚩᚠᚢᚱ᛫ᚠᛁᚱᚪ᛫ᚷᛖᚻᚹᛦᛚᚳᚢᛗ ᛋᚳᛖᚪᛚ᛫ᚦᛖᚪᚻ᛫ᛗᚪᚾᚾᚪ᛫ᚷᛖᚻᚹᛦᛚᚳ᛫ᛗᛁᚳᛚᚢᚾ᛫ᚻᛦᛏ᛫ᛞᚫᛚᚪᚾ ᚷᛁᚠ᛫ᚻᛖ᛫ᚹᛁᛚᛖ᛫ᚠᚩᚱ᛫ᛞᚱᛁᚻᛏᚾᛖ᛫ᛞᚩᛗᛖᛋ᛫ᚻᛚᛇᛏᚪᚾ᛬"
+
+BRUT="An preost wes on leoden, Laȝamon was ihoten He wes Leovenaðes sone -- liðe him be Drihten. He wonede at Ernleȝe at æðelen are chirechen, Uppen Sevarne staþe, sel þar him þuhte, Onfest Radestone, þer he bock radde."
+
+POLYTONIC="Τὴ γλῶσσα μοῦ ἔδωσαν ἑλληνικὴ τὸ σπίτι φτωχικὸ στὶς ἀμμουδιὲς τοῦ Ὁμήρου. Μονάχη ἔγνοια ἡ γλῶσσα μου στὶς ἀμμουδιὲς τοῦ Ὁμήρου."
+
+GEORGIAN="ვეპხის ტყაოსანი შოთა რუსთაველი ღმერთსი შემვედრე, ნუთუ კვლა დამხსნას სოფლისა შრომასა, ცეცხლს, წყალსა და მიწასა, ჰაერთა თანა მრომასა; მომცნეს ფრთენი და აღვფრინდე, მივჰხვდე მას ჩემსა ნდომასა, დღისით და ღამით ვჰხედვიდე მზისა ელვათა კრთომაასა."
+
+JAPANESE="私はガラスを食べられます。それは私を傷つけません。"
+
+echo "${SMILEYS} ${RUNE} ${BRUT} ${POLYTONIC} ${GEORGIAN} ${JAPANESE}"
