4a9595d4a0f965f6c23d8a3360ddd212a8527cd5
joolz <joolz@xs4all.nl>
2019-06-06 13:25:41 +0200

    Generate image to stop catalina whining. Only for test environments

---
+++ generateLiferayImage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source ~/bin/common.sh || exit 1
+
+# generate a small jpg called 1.0 in directory $1 (e.g. /opt/dxp/server/data/document_library/0/0/103565119.jpg)
+
+mkdir -p $1 || exit
+cd $1 || exit
+convert -size 100x100 xc:white blank.jpg
+mv blank.jpg 1.0
