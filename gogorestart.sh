f8cb0385d3f6ac88aab14e0bac7bfb66c63e86aa
joolz <joolz@xs4all.nl>
2019-06-20 11:46:27 +0200

    Restart OSGi bundle

---
+++ gogorestart.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+SERVICE=$1
+
+if [ -z "$SERVICE" ]; then
+	echo "Usage: $0 [name of osgi bundle to restart]"
+	exit 1
+fi
+
+RUNNING=0
+TEMPFILE=`mktemp`
+
+while [ $RUNNING -lt 1 ]; do
+   { echo "lb $SERVICE"; sleep 1; } | telnet localhost 11311 > $TEMPFILE
+   BUNDLE=$(cat $TEMPFILE | grep -i $SERVICE | grep Active | awk '{print $1}')
+   if [ -n "$BUNDLE" ]; then
+      STOPME="$(echo $BUNDLE | cut -d'|' -f1)"
+      { echo "stop $STOPME"; sleep 5; } | telnet localhost 11311 >> $TEMPFILE
+      { echo "start $STOPME"; sleep 5; } | telnet localhost 11311 >> $TEMPFILE
+      RUNNING=1
+   else
+      echo "Running: $RUNNING"
+   fi
+   sleep 1
+done
+
+rm -f $TEMPFILE
