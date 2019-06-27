9bd5fa769b52126e6087dbec2efe40dc0ac6f2af
joolz <joolz@xs4all.nl>
2019-06-27 14:39:20 +0200

    Add projects

--- allProjects.sh
+++ allProjects.sh
@@ -48,12 +48,14 @@ doIt nl-ou-dlwo-bridges
 doIt nl-ou-dlwo-ckeditor-config
 doIt nl-ou-dlwo-ckeditor-plugins
 doIt nl-ou-dlwo-collaborate
+doIt nl-ou-dlwo-common
 doIt nl-ou-dlwo-control-menu
 doIt nl-ou-dlwo-courseplan
 doIt nl-ou-dlwo-groupchat
 doIt nl-ou-dlwo-groupwall
 doIt nl-ou-dlwo-layouttemplate
 doIt nl-ou-dlwo-maildigester
+doIt nl-ou-dlwo-mb-common
 doIt nl-ou-dlwo-menu
 doIt nl-ou-dlwo-pagecloaker
 doIt nl-ou-dlwo-pagestructure
@@ -67,6 +69,7 @@ doIt nl-ou-dlwo-theme
 doIt nl-ou-dlwo-theme-contributor
 doIt nl-ou-dlwo-theme-control-panel
 doIt nl-ou-dlwo-translations
+doIt nl-ou-dlwo-user-common
 doIt nl-ou-dlwo-userprofile-portlet
 doIt nl.ou.yl.assessment
 doIt nl.ou.yl.domain
