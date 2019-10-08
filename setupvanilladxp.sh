9f0d5b3a5cef6e06f37d102bf02914678d98dc57
joolz <joolz@xs4all.nl>
2019-10-08 11:18:53 +0200

    New Maven repo location

--- setupvanilladxp.sh
+++ setupvanilladxp.sh
@@ -30,7 +30,7 @@ ROOTCLASSESDIR=$TOMCATDIR/webapps/ROOT/WEB-INF/classes
 SYSTEMPROPS=$ROOTCLASSESDIR/system-ext.properties
 
 logger "Remove own snapshots from Maven repo"
-find ~/.m2/repository -type f -name "nl*SNAPSHOT*" -exec rm -v {} \;
+find ~/tmp/m2_repository -type f -name "nl*SNAPSHOT*" -exec rm -v {} \;
 
 logger "Start installing vanilla DXP in $DXPSERVERDIR"
 START=$SECONDS
