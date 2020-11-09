e8cfbaae6aef0f4314d98d50e65f625168518973
Joolz <joolz@xs4all.nl>
2020-11-09 06:49:45 +0100

    Create servicebuilder project

---
+++ servicebuilderproject.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "Usage $0 PROJECTNAME"
+	exit 1
+fi
+
+mvn archetype:generate \
+		-DdependencyInjector=spring \
+		-DarchetypeGroupId=com.liferay \
+		-DarchetypeArtifactId=com.liferay.project.templates.service.builder \
+		-DinteractiveMode=false \
+		-DgroupId=nl.ou.dlwo \
+		-Dversion=1.0.0-SNAPSHOT \
+		-DaddOns=false \
+		-DbuildType=maven \
+		-Dproduct=portal \
+		-Dpackage=nl.ou.dlwo \
+		-DapiPath=-api \
+		-DliferayVersion=7.2+ \
+		-DartifactId=${1}
