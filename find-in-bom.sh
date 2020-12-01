f7cbfab473182c097af2c328d5ec37a06511ee98
Joolz <joolz@xs4all.nl>
2020-12-01 08:14:47 +0100

    Work on 72 logging

--- find-in-bom.sh
+++ find-in-bom.sh
@@ -5,7 +5,12 @@ source ~/bin/common.sh || exit 1
 FILE1=/home/jal/tmp/m2_repository/com/liferay/portal/release.dxp.bom/${DXP72BOMVERSION}/release.dxp.bom-${DXP72BOMVERSION}.pom
 FILE2=/home/jal/tmp/m2_repository/com/liferay/portal/release.dxp.bom.compile.only/${DXP72BOMVERSION}/release.dxp.bom.compile.only-${DXP72BOMVERSION}.pom
 FILE3=/home/jal/tmp/m2_repository/com/liferay/portal/release.dxp.bom.third.party/${DXP72BOMVERSION}/release.dxp.bom.third.party-${DXP72BOMVERSION}.pom
+FILE4=/home/jal/workspace/nl.ou.yl.bom/pom.xml
 
+echo "In Liferay BOMs -----------------------------------------------"
 grep -B 2 -A 2 "$1" "${FILE1}"
 grep -B 2 -A 2 "$1" "${FILE2}"
 grep -B 2 -A 2 "$1" "${FILE3}"
+
+echo "In yOUlearn BOM -----------------------------------------------"
+grep -B 2 -A 2 "$1" "${FILE4}"
