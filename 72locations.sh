9e7a6014175d95719c58b658e7a295599909ee36
Jules Alberts <jules.alberts@ou.nl>
2021-01-27 08:10:22 +0100

    Some repairs after refactoring

--- 72locations.sh
+++ 72locations.sh
@@ -2,7 +2,7 @@ DXPBASEDIR=/opt/dxp
 DXPSERVERDIR=$DXPBASEDIR/server
 DXPTOMCATDIR=$DXPSERVERDIR/tomcat-9.0.33
 
-DXPDOWNLOADSDIR=$DXPBASEDIR/resources
+DXPDOWNLOADSDIR=/home/jal/Nextcloud/Downloads/72resources
 DXPPATCHESDIR=$DXPDOWNLOADSDIR/patches
 
 DXPSOURCEZIP=liferay-dxp-src-7.2.10.3-sp3-202009100727.zip
@@ -11,7 +11,7 @@ DXPSOURCEPHYSICALDIR=liferay-dxp-src-7.2.10.3-sp3
 DXPSERVERZIP=liferay-dxp-tomcat-7.2.10.3-sp3-20200910120006703.tar.gz
 DXPSERVERPHYSICALDIR=liferay-dxp-7.2.10.3-sp3
 
-DXP72PATCHLEVEL="dxp-10" # dir under patches where the combinations are kept
+DXPPATCHLEVEL="dxp-10" # dir under patches where the combinations are kept
 
 DXPACTIVATIONKEY="$DXPDOWNLOADSDIR/activation-key-digitalenterprisedevelopment-7.2-developeractivationkeys.xml"
 
