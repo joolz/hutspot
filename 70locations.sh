f683f131c83a599162a95803ae6c1d7294c6fb3a
Jules Alberts <jules.alberts@ou.nl>
2021-02-16 14:46:46 +0100

    70 locations

---
+++ 70locations.sh
@@ -0,0 +1,18 @@
+DXPBASEDIR=/opt/70dxp
+DXPSERVERDIR=$DXPBASEDIR/server
+DXPTOMCATDIR=$DXPSERVERDIR/tomcat-8.0.32
+
+DXPDOWNLOADSDIR=/home/jal/Nextcloud/Downloads/70dxp
+DXPPATCHESDIR=$DXPDOWNLOADSDIR/patches
+
+DXPSOURCEZIP=liferay-dxp-digital-enterprise-src-7.0.10.11-sp11-20190506123513875.zip
+DXPSOURCEPHYSICALDIR=liferay-dxp-digital-enterprise-src-7.0.10.11-sp11
+
+DXPSERVERZIP=liferay-dxp-digital-enterprise-tomcat-7.0.10.11-sp11-20190506123513875.zip
+DXPSERVERPHYSICALDIR=liferay-dxp-digital-enterprise-7.0.10.11-sp11
+
+DXPPATCHLEVEL="de-93" # dir under patches where the combinations are kept
+
+DXPACTIVATIONKEY="$DXPDOWNLOADSDIR/activation-key-digitalenterprisedevelopment-7.0-developeractivationkeys.xml"
+
+DXPBRANCHNAME="default"
