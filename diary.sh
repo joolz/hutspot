41a26f0d1e140454df3aace5c1d215e735a9340f
Jules Alberts <jules.alberts@ou.nl>
2021-03-23 12:19:05 +0100

    Try out typora

--- diary.sh
+++ diary.sh
@@ -7,20 +7,27 @@ DIARYBASE=~/Nextcloud/diary
 YEAR=`date +%Y`
 WEEK=`date +%V` # https://stackoverflow.com/questions/19113732/cenos-shows-incorrect-week-number
 
+EDITOR_VI='vi -o'
+EDITOR_OTHER=typora
+
 DAY_OF_WEEK=`date +%u`
 PREVIOUS_WEEK=`date -d 'last week' +%V`
 PREVIOUS_YEAR=`date -d 'last week' +%Y`
 
 mkdir -p $DIARYBASE/$YEAR
 
-DIARYEDITOR='vi -o'
+DIARYEDITOR=${EDITOR_OTHER}
 
-DIARY=${DIARYBASE}/${YEAR}/${YEAR}_week_${WEEK}.md
-PREVIOUS_DIARY=${DIARYBASE}/${PREVIOUS_YEAR}/${PREVIOUS_YEAR}_week_${PREVIOUS_WEEK}.md
+if [ "$DIARYEDITOR" == "${EDITOR_VI}" ]; then
+	DIARY=${DIARYBASE}/${YEAR}/${YEAR}_week_${WEEK}.md
+	PREVIOUS_DIARY=${DIARYBASE}/${PREVIOUS_YEAR}/${PREVIOUS_YEAR}_week_${PREVIOUS_WEEK}.md
 
-if [ "$DAY_OF_WEEK" == "1" ]; then
-	${DIARYEDITOR} -o $PREVIOUS_DIARY $DIARY
+	if [ "$DAY_OF_WEEK" == "1" ]; then
+		${DIARYEDITOR} -o $PREVIOUS_DIARY $DIARY
+	else
+		${DIARYEDITOR} $DIARY
+	fi
 else
-	${DIARYEDITOR} $DIARY
+		cd $DIARYBASE/$YEAR
+		typora .
 fi
-
