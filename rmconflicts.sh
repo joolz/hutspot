de970b868684b20b405dfdbef37e0017e0fd8936
Jules Alberts <jules.alberts@ou.nl>
2021-04-30 12:53:39 +0200

    Exclude root owned dir

--- rmconflicts.sh
+++ rmconflicts.sh
@@ -5,8 +5,18 @@ source $CREDSFILE || exit 1
 
 if [ "${1}" == "-b" ]; then
 	# background for selected crap
-	find ${NEXTCLOUDDIR} -regex ".*Microsoft Teams.*conflicted copy.*" -exec rm -v {} \;
+	find \
+		${NEXTCLOUDDIR} \
+		-path ${NEXTCLOUDDIR}/rasa-compose/db -prune -false -o \
+		-regex ".*Microsoft Teams.*conflicted copy.*" \
+		-exec rm -v {} \;
 else
 	# interactive for all conflicted copies
-	find ${NEXTCLOUDDIR} -iname "*conflicted copy*" -exec rm -iv {} \;
+	find \
+		${NEXTCLOUDDIR} \
+		-path ${NEXTCLOUDDIR}/rasa-compose/db -prune -false -o \
+		-iname "*conflicted copy*" \
+		-exec rm -iv {} \;
 fi
+
+#		-not -path "${NEXTCLOUDDIR}/rasa-compose/db/*" \
